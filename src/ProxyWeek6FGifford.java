bab05da5888cfb3ef1795762478bc5b9c9800b67
fdgifford3 <fdgifford3@gmail.com>
2019-10-04 18:28:08 -0500

    Week 6 CPSC 600 Assignment - Proxy Pattern Fred Gifford

---
+++ src/ProxyWeek6FGifford.java
@@ -0,0 +1,16 @@
+/*
+ * Frederick Gifford
+ * CPSC-60000 Week 6 Proxy Pattern
+ * There were several interesting variations of the proxy pattern, and I chose
+ * A dynamic proxy to introduce access protection for a student or a teacher accessing a profile
+ * 10.4.2019
+ * Prof Nowak
+ */
+public class ProxyWeek6FGifford {
+
+	public static void main(String[] args) {
+		System.out.println("Frederick Gifford\nCPSC-60000\nWeek 6 Proxy Pattern\n");
+
+	}
+
+}
